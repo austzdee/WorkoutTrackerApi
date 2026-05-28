3a733afe3749e43ba289fb19f96e919925653df6
Daniel Okafor <austz@Mac.lan>
2026-05-28 19:31:45 +0100

    Add EF Core SQLite tooling

--- obj/Debug/net10.0/WorkoutTrackerApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/WorkoutTrackerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkoutTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89dcfd59df6692f4bb686da6ffd1b77d629d19ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkoutTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkoutTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
